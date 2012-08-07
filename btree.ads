bf417dfe704a8332b899d6bbe48fdcb5accff4f2
William Dignazio <slackwill@csh.rit.edu>
2012-08-06 23:41:57 -0400

    Working on the base types for tree structure in Ada

--- btree.ads
+++ btree.ads
@@ -2,5 +2,11 @@ with 	Ada.Text_IO;
 use 	Ada.Text_IO; 
 
 package btree is 
+	type Node;
+	type BTree is access Node; 
+	type Node is record 
+		Key : Integer; 
+		Child : BTree; 
+	end record; 
 	procedure Hello; --Simple Hello World...
 end btree;
